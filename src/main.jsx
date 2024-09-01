a11bfe15c1114c359c7becec56fc793a2394a01f
ralbertcoder <ralbertvch@gmail.com>
2024-09-01 14:33:05 -0600

    Aplicacion para el curso de React

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BuscadorPeliculas } from './BuscadorPeliculas.jsx'
+import './styles/buscador.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BuscadorPeliculas></BuscadorPeliculas>
+  </StrictMode>,
+)
+ 
